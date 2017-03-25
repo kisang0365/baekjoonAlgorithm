7242cae10351cf49bbabe38397c122bcb03fb77b
System Administrator <root@gisang-iui-MacBook-Pro.local>
2017-03-25 18:35:19 +0900

    algo

---
+++ 11719.cpp
@@ -0,0 +1,26 @@
+/*
+ * 11719.cpp
+ *
+ *  Created on: 2017. 3. 25.
+ *      Author: chokisang
+ */
+
+
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(void){
+
+	string str ="";
+    int i=0;
+	while(i<100){
+
+		getline(cin,str);
+		cout<<str<<endl;
+        i++;
+	}
+}
+

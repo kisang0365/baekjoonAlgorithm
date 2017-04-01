e5f267fdcd57eb6782f3d66eada6fcc5b38e42c7
System Administrator <root@gisang-iui-MacBook-Pro.local>
2017-04-01 14:43:20 +0900

    /

---
+++ 1008.cpp
@@ -0,0 +1,24 @@
+/*
+ * 1008.cpp
+ *
+ *  Created on: 2017. 4. 1.
+ *      Author: chokisang
+ */
+
+
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+
+	int A, B;
+
+	cin >> A >> B;
+	cout.precision(9);
+	cout<<fixed << (double)A/B <<endl;
+
+
+}
+

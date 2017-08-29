bf792f60c26e783621094d7627f279c1ff900067
System Administrator <root@gisang-iui-MacBook-Pro.local>
2017-08-29 14:57:36 +0900

    backjoon

--- 14503.cpp
+++ 14503.cpp
@@ -1,86 +1,77 @@
-/*
- * 14503.cpp
- *
- *  Created on: 2017. 7. 26.
- *      Author: chokisang
- */
-
 #include<iostream>
 
 using namespace std;
 
 int N, M;
-int Board[51][51];
-int x, y, d;
-int ans = 0;
+int r, c, d;
+//북0 동 1 남2 서 3
+//북 -> 서
+//동 -> 북
+// 남 -> 동
+// 서 -> 남
 
 int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, -1, 0, 1};
 
-void checking(int sx, int sy){
+int Wall[51][51];
+int visit[51][51] = {0, };
+int ans = 0;
 
-	if(Board[sx][sy] == 0){
-		Board[sx][sy] = 2;
-		ans++;
-	}
+void find(int x, int y){
+    
+    if(visit[x][y] == 0){
+        visit[x][y] = 1;
+        ans++;
+    }
 
-	int cnt = 1;
-	int nx;
-	int ny;
-	while(cnt <= 4){
+    int newX, newY;
+    int cnt = 0;
+    while(cnt < 4){
+        
+        d = (d+1)%4;
 
-		d = (d+1)%4;
+        newX = dx[d]+x;
+        newY = dy[d]+y;
 
-		nx = sx + dx[d];
-		ny = sy + dy[d];
-		if(nx>=0 && nx<N && ny >= 0 && ny<M){
-			if(Board[nx][ny] == 0){
-				break;
-			}
-		}
-		cnt++;
-	}
-	if(cnt > 4){
-		bool ok = true;
-		int a = dx[d];
-		int b = dy[d];
-
-		if(sx-a <0 || sy-b < 0 || sx-a >=N || sy-b >=M || Board[sx-a][sy-b] == 1){
-			ok = false;
-		}
-
-		if(ok){
-			checking(sx-a, sy-b);
-		}
-		else{
-			return;
-		}
-	}
-	else{
-		checking(nx, ny);
-	}
-
+        if(0<= newX && newX < N && 0<=newY && newY < M){
+            if(visit[newX][newY] == 0){
+                break;
+            }
+        }
+        cnt++;
+    }
+    
+    if(cnt >= 4){
+        newX = -dx[d]+x;
+        newY = -dy[d]+y;
+        
+        if(newX >= 0 && newX < N && newY >= 0 && newY < M){
+            if(Wall[newX][newY] == 0){
+                find(newX, newY);
+            }
+        }
+    }
+    else{
+        find(newX, newY);
+    }
 }
-
-//북->서, 서-> 남, 남->동, 동->북
-//0 , 1, 2, 3.
 
 int main(){
 
-	cin >> N >> M;
-	cin >> x >> y >> d;
+    cin >> N >> M;
+    cin >> r >> c >> d;
 
-	if(d==3) d=1;
-	else if(d==1) d=3;
+    if( d == 3) d = 1;
+    else if(d == 1) d = 3;
 
-	for(int i=0; i<N; i++){
-		for(int j=0; j<M; j++){
-			cin >> Board[i][j];
-		}
-	}
-	checking(x,y);
-	cout<<ans<<endl;
-
+    for(int i=0; i<N; i++){
+        for(int j=0; j<M; j++){
+            cin >> Wall[i][j];
+            visit[i][j] = 0;
+            if(Wall[i][j] == 1) visit[i][j] = 1;
+        }
+    }
+    find(r,c);
+    cout<<ans<<endl;
+    return 0;
 }
-
-
